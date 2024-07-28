c9c84772594aa3b9fe650c75a3e8775d1301819f
hradhi101 <146386179+hradhi101@users.noreply.github.com>
2024-07-28 00:59:05 -0400

    Login front end implemented

--- onsync_frontend/lib/Login.dart
+++ onsync_frontend/lib/Login.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: LoginScreen(),
       ),
@@ -16,26 +18,28 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF01204E),
+      backgroundColor: const Color(0xFF01204E),
       body: Center(
-        child: Container(
-          width: 430,
-          height: 932,
-          margin: EdgeInsets.only(top: 1977, left: -298),
-          decoration: BoxDecoration(
-            color: Color(0xFF01204E),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(35),
+        child: SingleChildScrollView(
+          child: Container(
+            width: 430,
+            height: 932,
+            decoration: const BoxDecoration(
+              color: Color(0xFF01204E),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              SizedBox(height: 179),
-              Center(
-                child: Text(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 179),
+                const Text(
                   'Login',
                   style: TextStyle(
                     fontFamily: 'Roboto',
@@ -43,12 +47,11 @@ class LoginScreen extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                     letterSpacing: 0.025,
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: Text(
+                const SizedBox(height: 20),
+                const Text(
                   'Sign in to your account',
                   style: TextStyle(
                     fontFamily: 'Roboto',
@@ -57,91 +60,88 @@ class LoginScreen extends StatelessWidget {
                     color: Colors.white,
                     letterSpacing: 0.025,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 60),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 37.0),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 356,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Color(0xFFC19A6B)),
-                      ),
-                      child: Center(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Enter your username',
-                            hintStyle: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 0.025,
+                const SizedBox(height: 60),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 37.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 356,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xFFC19A6B)),
+                        ),
+                        child: const Center(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter your username',
+                              hintStyle: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.025,
+                              ),
                             ),
-                            textAlign: TextAlign.center,
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      width: 356,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Color(0xFFC19A6B)),
-                      ),
-                      child: const Center(
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Enter your password',
-                            hintStyle: TextStyle(
-                              fontFamily: 'Roboto',
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              letterSpacing: 0.025,
+                      const SizedBox(height: 20),
+                      Container(
+                        width: 356,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xFFC19A6B)),
+                        ),
+                        child: const Center(
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Enter your password',
+                              hintStyle: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                letterSpacing: 0.025,
+                              ),
                             ),
-                            textAlign: TextAlign.center,
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 40),
-              Container(
-                width: 284,
-                height: 44,
-                decoration: BoxDecoration(
-                  color: Color(0xFFC19A6B),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(35),
+                    ],
                   ),
                 ),
-                child: TextButton(
-                  onPressed: () {
-                    // Navigate to Dashboard
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 0.025,
+                const SizedBox(height: 40),
+                Container(
+                  width: 284,
+                  height: 44,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFC19A6B),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      // Navigate to Dashboard
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 0.025,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 80),
-              Center(
-                child: Text(
+                const SizedBox(height: 80),
+                const Text(
                   'Don’t have an account?',
                   style: TextStyle(
                     fontFamily: 'Roboto',
@@ -150,35 +150,36 @@ class LoginScreen extends StatelessWidget {
                     color: Colors.white,
                     letterSpacing: 0.025,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 284,
-                height: 44,
-                decoration: BoxDecoration(
-                  color: Color(0xFFC19A6B),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(35),
+                const SizedBox(height: 20),
+                Container(
+                  width: 284,
+                  height: 44,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFC19A6B),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35),
+                    ),
                   ),
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    // Navigate to Create Account
-                  },
-                  child: Text(
-                    'Create Account',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      letterSpacing: 0.025,
+                  child: TextButton(
+                    onPressed: () {
+                      // Navigate to Create Account
+                    },
+                    child: const Text(
+                      'Create Account',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 0.025,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
